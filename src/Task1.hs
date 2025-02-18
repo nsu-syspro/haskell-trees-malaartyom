e2c3490c6632639e3678c99f2d7c792e95e6e5d6
malaartyom <malarenkoart@bk.ru>
2025-02-18 15:28:30 +0700

    Add commets to Task1.hs

--- src/Task1.hs
+++ src/Task1.hs
@@ -38,26 +38,60 @@ torder :: Order    -- ^ Order of resulting traversal
        -> Maybe a  -- ^ Optional leaf value
        -> Tree a   -- ^ Tree to traverse
        -> [a]      -- ^ List of values in specified order
+      
 torder order leaf tree = case order of
         PreOrder -> pretorder leaf tree
         InOrder -> itorder leaf tree
         PostOrder -> postorder leaf tree
 
+-- | Returns empty list of Maybe is Nothing, othrwise (in case of Just x) returns [x]
+-- 
+-- Usage example:
+-- 
+-- >>> maybeToList (Just 1)
+-- [1]
+-- >>> maybeToList Nothing
+-- []
+
+
 maybeToList :: Maybe a -> [a]
 maybeToList Nothing = []
 maybeToList (Just x) = [x]
 
--- TODO: Write comments 
+-- | Returns list of tree's nodes in pre-order
+-- 
+-- Usage example:
+-- 
+-- >>> pretorder (Just '.') (Branch 'A' Leaf (Branch 'B' Leaf Leaf))
+-- "A.B.."  
+
 pretorder :: Maybe a -> Tree a -> [a]
 pretorder leaf tree = case tree of
         Leaf           -> maybeToList leaf
         (Branch a l r) -> [a] ++ pretorder leaf l ++ pretorder leaf r
+
+
+
+-- | Returns list of tree's nodes in in-order 
+--
+-- Usage example:
+--
+-- >>> itorder  (Just '.') (Branch 'A' Leaf (Branch 'B' Leaf Leaf))
+-- ".A.B."
 
 itorder :: Maybe a -> Tree a -> [a]
 itorder leaf tree = case tree of
         Leaf           -> maybeToList leaf
         (Branch a l r) -> itorder leaf l ++ [a] ++ itorder leaf r
 
+
+-- | Returns list of tree's nodes in post-order
+--
+-- Usage example:
+--
+-- >>> postorder  (Just '.') (Branch 'A' Leaf (Branch 'B' Leaf Leaf))
+-- "...BA"
+ 
 
 postorder :: Maybe a -> Tree a -> [a]
 postorder leaf tree = case tree of
@@ -75,12 +109,8 @@ postorder leaf tree = case tree of
 -- ".|.C.|.A.B."
 -- >>> forder PostOrder (Just '|') (Just '.') [Leaf, Branch 'C' Leaf Leaf, Branch 'A' Leaf (Branch 'B' Leaf Leaf)]
 -- ".|..C|...BA"
-
-intercalate :: [a] -> [[a]] -> [a]
-intercalate _ [] = []
-intercalate _ [x] = x
-intercalate sep (x:xs) = x ++ sep ++ intercalate sep xs
 --
+
 forder :: Order     -- ^ Order of tree traversal
        -> Maybe a   -- ^ Optional separator between resulting tree orders
        -> Maybe a   -- ^ Optional leaf value
@@ -89,3 +119,19 @@ forder :: Order     -- ^ Order of tree traversal
 forder order sep leaf forest = case sep of
       Nothing  -> concatMap (torder order leaf) forest
       (Just x) -> intercalate [x] (map (torder order leaf) forest)
+-- | intercalate xs xss is equivalent to (concat (intersperse xs xss)).
+--  It inserts the list xs in between the lists in xss and concatenates the result.
+--
+-- Usage example:
+-- 
+-- >>> intercalate ", " ["Lorem", "ipsum", "dolor"]
+-- "Lorem, ipsum, dolor"
+-- >>> intercalate [0, 1] [[2, 3], [4, 5, 6], []]
+-- [2,3,0,1,4,5,6,0,1]
+-- >>> intercalate [1, 2, 3] [[], []]
+-- [1, 2, 3]
+
+intercalate :: [a] -> [[a]] -> [a]
+intercalate _ [] = []
+intercalate _ [x] = x
+intercalate sep (x:xs) = x ++ sep ++ intercalate sep xs
